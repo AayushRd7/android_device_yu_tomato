21b588147fd041b65617bd59660cf9df652def0d
ghostrider96 <lensflare21@gmail.com>
2016-10-16 14:33:28 +0200

    add TWRP configs

---
+++ board/twrp.mk
@@ -0,0 +1,7 @@
+# TWRP Build Flags
+TARGET_RECOVERY_FSTAB := device/yu/tomato/rootdir/etc/twrp.fstab
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_CRYPTO := false
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TARGET_RECOVERY_QCOM_RTC_FIX := true
